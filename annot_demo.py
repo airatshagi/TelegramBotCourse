dd4e89e15fdf63829eb03c4f19da4f7ef38e1b02
Ayrat Shagi <fakel_cool_15@mail.ru>
2025-07-15 00:07:29 +0400

    Доработка 'annot_demo.py'

--- annot_demo.py
+++ annot_demo.py
@@ -1,3 +1,10 @@
 def say_something(number: int, word: str) -> str:
     word = word.capitalize()
     return word * number
+
+
+def get_string(string: str, number: int) -> str:
+    return string * number
+
+
+print(get_string.__annotations__)
